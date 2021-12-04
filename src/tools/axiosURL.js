29a7a2615dad7fa4eb6da2d73ecf7a190eb36965
Yamina <yamina@Air-de-Yamina.lan>
2021-12-04 23:05:53 +0100

    axios update

--- src/tools/axiosURL.js
+++ src/tools/axiosURL.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const connectBack = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://helpr-2021.herokuapp.com/',
 	headers: {
 		'Content-Type': 'application/json'
 	}
